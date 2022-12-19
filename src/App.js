38b19bc2b78ac4c4a46c4b628a6d79872fcbaf78
Abhimanyu <43077595+a12baghel2@users.noreply.github.com>
2022-12-19 18:01:59 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+//import "bootstrap/dist/css/bootstrap.min.css";
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Up and working</p>
+    </div>
+  );
+}
+
+export default App;
